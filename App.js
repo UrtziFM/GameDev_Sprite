0c8e9bcda5be1ccbaa14a6f94fcc90e88e60b267
u100247 <u100247@MM0012B7.local>
2022-11-04 19:43:46 +0100

    animation working time 18:00

--- App.js
+++ App.js
@@ -4,3 +4,23 @@ const canvasWidth  = canvas.width = 600;
 const canvasHeight = canvas.height = 600;
 
 const  playerImage = new Image();
+playerImage.src = "Assets/shadow_dog.png";
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+
+function animate(){
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    //ctx.fillRect(100, 50, 100, 100);
+    //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+    ctx.drawImage(playerImage, frameX*spriteWidth, frameY*spriteHeight,
+        spriteHeight, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+        if( frameX < 6){
+            frameX++;
+        } else {
+            frameX = 0;
+        }
+        requestAnimationFrame(animate);
+};
+animate();
