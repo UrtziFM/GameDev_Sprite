ce569de70ef1d9cf35f2fa1996737740ff73d4a8
u100247 <u100247@MM0012B7.local>
2022-11-05 11:21:45 +0100

    Time 35:44, data structure

--- App.js
+++ App.js
@@ -9,18 +9,43 @@ const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    }
+];
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for  (let j = 0; j < state.frames; j++){
+        let positionX = j *  spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y:positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+
+console.log(animationStates);
 
 function animate(){
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    //ctx.fillRect(100, 50, 100, 100);
-    //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-    ctx.drawImage(playerImage, frameX*spriteWidth, frameY*spriteHeight,
-        spriteHeight, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-        if( frameX < 6){
-            frameX++;
-        } else {
-            frameX = 0;
-        }
+    let position = Math.floor(gameFrame/staggerFrames) % 6;
+    frameX = spriteWidth * position;
+    ctx.drawImage(playerImage, frameX, frameY*spriteHeight,
+        spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+        
+        gameFrame++;
+
         requestAnimationFrame(animate);
 };
 animate();
