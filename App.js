4174172740855950cfb7bcd99f927fb62f0dc556
u100247 <u100247@MM0012B7.local>
2022-11-06 11:50:04 +0100

    Sprite Animation is finished, it seems is working correctly, let's it merge

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
+let playerState = "run";
+const dropdown = document.getElementById("animations");
+dropdown.addEventListener("change", function(e){
+    playerState = e.target.value;
+});
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const canvasWidth  = canvas.width = 600;
@@ -7,11 +13,10 @@ const  playerImage = new Image();
 playerImage.src = "Assets/shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
 const spriteAnimations = [];
+
 const animationStates = [
     {
         name: 'idle',
@@ -20,6 +25,38 @@ const animationStates = [
     {
         name: 'jump',
         frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
     }
 ];
 
@@ -39,9 +76,11 @@ console.log(animationStates);
 
 function animate(){
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations["idle"].loc.length;
-    frameX = spriteWidth * position;
-    ctx.drawImage(playerImage, frameX, frameY*spriteHeight,
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+
+    ctx.drawImage(playerImage, frameX, frameY,
         spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
         
         gameFrame++;
